7eb827a399916fef4c4050a7c81b4b2d3abcd35d
unknown <Nicejerem@hotmail.fr>
2018-07-12 16:18:21 +0200

    Terminaison du TP

--- src/test/java/TP8.java
+++ src/test/java/TP8.java
@@ -1,3 +1,5 @@
+import org.hamcrest.core.Is;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -5,6 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.interactions.SourceType;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -20,18 +23,33 @@ public class TP8 {
 
     @Test
     public void t1() {
-        String idDivMenu = "nav-link-shopall";
-        WebElement divMenu = driver.findElement(By.id(idDivMenu));
+
+        WebElement divMenu = driver.findElement(By.xpath("//*[@id=\"primary-menu\"]/li[8]/a"));
         Actions actions = new Actions(driver);
         actions.moveToElement(divMenu);
-        WebDriverWait wait = new WebDriverWait(driver, 5);
         actions.build().perform();
-        WebElement span = driver.findElement(By.cssSelector("span[data-nav-panelkey=\"VideogamesConsolesPanel\"]"));
-        wait.until( ExpectedConditions.visibilityOf(span));
-        actions.moveToElement(span);
+        WebDriverWait wait = new WebDriverWait(driver,5);
+        WebElement iframeMenu = driver.findElement(By.xpath("/html/body/div[1]/div[1]/header/nav/ul/li[8]/ul/li[7]/a/span[1]/span/span"));
+        wait.until(ExpectedConditions.visibilityOf(iframeMenu));
+        actions.moveToElement(iframeMenu);
+        actions.click();
         actions.build().perform();
-        WebElement link = driver.findElement( By.xpath("//span[text()=\"Nintendo Switch\"]"));
-        link.click();
+
+        wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt("IF1"));
+        String H1_iframe1= driver.findElement(By.xpath("//*[@id=\"content\"]/div[1]/div/div/div/div[2]/div/h1")).getText();
+        Assert.assertThat(H1_iframe1, Is.is("Practice Automation Form"));
+
+        driver.switchTo().defaultContent();
+        wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt("IF2"));
+        String H1_iframe2= driver.findElement(By.xpath("//*[@id=\"post-9\"]/header/h1")).getText();
+        Assert.assertThat(H1_iframe2, Is.is("Home"));
+
+        driver.switchTo().defaultContent();
+        WebElement H1 =driver.findElement(By.xpath("//*[@id=\"content\"]/h1"));
+        //wait.until(ExpectedConditions.visibilityOf(H1));
+        String h1Text = H1.getText();
+        Assert.assertThat(h1Text, Is.is("Sample Iframe page"));
+        System.out.println(" Le text du H1 :"+h1Text);
     }
 
 
